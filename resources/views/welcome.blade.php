4422dfe9a5779ac137574e259ea123cfc3714fe5
Jesús Alberto <speed_chucho@hotmail.com>
2016-09-29 00:49:24 -0500

    Laravel framework application
    first application in PHP with Laravel Framework

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            footer {
+                margin: 0;
+                text-align: center;
+                font-family: 'Arial';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Bienvenido/Welcome</div>
+            </div>
+            <footer>
+                @PuntoCreativoStudio. 2016.
+            </footer>
+        </div>
+        
+    </body>
+</html>
